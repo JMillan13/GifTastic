6c9509aae71ec1f046b292cb43428085f7a4eb51
Joseph Millan <https://github.com/JMillan13>
2019-03-30 22:30:24 -0400

    adjustments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,9 +11,9 @@ $(document).ready(function(){
 
       .done(function(response) {
 
-          console.log(response)
+       console.log(response)
 
-            var results = response.data;
+       var results = response.data;
 
             for (var i = 0; i < results.length; i++) {
 
@@ -21,115 +21,115 @@ $(document).ready(function(){
 
             var p =$('<p/>');
 
-                 p.text(results[i].rating);
+            p.text(results[i].rating);
 
             var playerImage = $('<img/>');
 
-                 playerImage.addClass('plyImg')
+            playerImage.addClass('plyImg')
 
                    playerImage.attr('src', results[i].images.fixed_height.url);
                    playerImage.attr('data-still', results[i].images.fixed_height_still.url)
                    playerImage.attr('data-animate', results[i].images.fixed_height.url)
 
-                   .attr('data-state', 'still');
+            .attr('data-state', 'still');
 
-                     playerDiv.append(p);
+                   playerDiv.append(p);
 
-                      playerDiv.append(playerImage);
+                   playerDiv.append(playerImage);
 
-                       playerDiv.prependTo($('#gifs'));
+                   playerDiv.prependTo($('#gifs'));
                              }
-                             $('.plyImg').on('click', function() {
+             $('.plyImg').on('click', function() {
 
-                              var state = $(this).attr('data-state');
-                                  console.log(this);
+                   var state = $(this).attr('data-state');
+                     console.log(this);
 
-                              if (state == 'still') {
+                      if (state == 'still') {
 
-                                 $(this).attr('src', $(this).data('animate'));
+                        $(this).attr('src', $(this).data('animate'));
 
-                                 $(this).attr('data-state', 'animate');
+                        $(this).attr('data-state', 'animate');
 
-                             } else {
+                    } else {
 
-                                 $(this).attr('src', $(this).data('still'));
+                         $(this).attr('src', $(this).data('still'));
 
-                                 $(this).attr('data-state', 'still');
+                        $(this).attr('data-state', 'still');
                                         }
                                     });
                                 });
                         });
-                        var player = [''];
+                    var player = [''];
 
-                        $('#theButton').on('click', function(){
-                          var playerButton = $("#gif-input").val();
+            $('#theButton').on('click', function(){
+                    var playerButton = $("#gif-input").val();
                              //adds the new player
 
-                           var newButton = $("<button/>").addClass( "btn btn-info player").attr('data-name',playerButton).html(playerButton).css({'margin': '5px'});
+                    var newButton = $("<button/>").addClass( "btn btn-info player").attr('data-name',playerButton).html(playerButton).css({'margin': '5px'});
 
-                           $("#playerbuttons").append(newButton);
-                           console.log("Work");
+            $("#playerbuttons").append(newButton);
+                    console.log("Work");
 
-                           queryURL = "https://api.giphy.com/v1/gifs/search?q=" + playerButton + "&api_key=dc6zaTOxFJmzC&limit=10";
-                           console.log(playerButton);
+                    queryURL = "https://api.giphy.com/v1/gifs/search?q=" + playerButton + "&api_key=dc6zaTOxFJmzC&limit=10";
+                    console.log(playerButton);
 
-                       $.ajax({
-                             url: queryURL,
-                             method: 'GET'
+            $.ajax({
+                   url: queryURL,
+                   method: 'GET'
                              })
 
-                        .done(function(response) {
+                  .done(function(response) {
 
-                           var results = response.data;
+                      var results = response.data;
 
-                        for (var i = 0; i < results.length; i++) {
+                  for (var i = 0; i < results.length; i++) {
 
-                             var playerDiv = $('<div/>');
+                       var playerDiv = $('<div/>');
 
-                               var p =$('<p/>');
+                          var p =$('<p/>');
 
-                           p.text(results[i].rating);
+                            p.text(results[i].rating);
 
-                              var playerImage = $('<img/>');
+                        var playerImage = $('<img/>');
 
-                                playerImage.addClass('plyImg')
+                            playerImage.addClass('plyImg')
 
-                                  playerImage.attr('src', results[i].images.fixed_height_still.url);
+                            playerImage.attr('src', results[i].images.fixed_height_still.url);
 
-                                    playerImage.attr('data-still', results[i].images.fixed_height_still.url)
+                            playerImage.attr('data-still', results[i].images.fixed_height_still.url)
 
-                                     playerImage.attr('data-animate', results[i].images.fixed_height.url)
-                                     .attr('data-state', 'still');
+                            playerImage.attr('data-animate', results[i].images.fixed_height.url)
+                       .attr('data-state', 'still');
 
-                                     playerDiv.append(p);
+                            playerDiv.append(p);
 
-                                       playerDiv.append(playerImage);
+                            playerDiv.append(playerImage);
 
-                                         playerDiv.prependTo($('#gifs'));
+                            playerDiv.prependTo($('#gifs'));
                                           }
 
-                                $('.plyImg').on('click', function() {
+                  $('.plyImg').on('click', function() {
 
-                                       var state = $(this).attr('data-state');
+                        var state = $(this).attr('data-state');
 
-                                       console.log(this);
+                         console.log(this);
 
-                                if (state == 'still') {
+                             if (state == 'still') {
 
-                                       $(this).attr('src', $(this).data('animate'));
+                                $(this).attr('src', $(this).data('animate'));
 
-                                       $(this).attr('data-state', 'animate');
+                                $(this).attr('data-state', 'animate');
 
                               } else {
 
-                                       $(this).attr('src', $(this).data('still'));
+                                 $(this).attr('src', $(this).data('still'));
 
-                                       $(this).attr('data-state', 'still');
+                                  $(this).attr('data-state', 'still');
                                               }
                                           });
                                       });
 
-                              $("#gif-input").val("");
+                  $("#gif-input").val("");
 
                                 return false;
 
