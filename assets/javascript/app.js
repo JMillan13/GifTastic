adf7ad292fb9e34206998f38969ed4811f7bd93d
Joseph Millan <https://github.com/JMillan13>
2019-03-30 20:00:27 -0400

    Update app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+  $('button').on('click', function() {
+      var player = $(this).data('name');
+      var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+      $.ajax({
+          url: queryURL,
+          method: 'GET'
+      })
+
+      .done(function(response) {
+
+          console.log(response)
+
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+            var playerDiv = $('<div/>');
+
+            var p =$('<p/>');
+
+                 p.text(results[i].rating);
+
+            var playerImage = $('<img/>');
+
+                 playerImage.addClass('plyImg')
+
+                   playerImage.attr('src', results[i].images.fixed_height.url);
